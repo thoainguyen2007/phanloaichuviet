f350ed1809da1627d3225b4047541f480079abd6
thoainguyen2007 <125574535+thoainguyen2007@users.noreply.github.com>
2023-05-05 15:29:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from tensorflow import keras
+model_load = keras.models.load_model('something(1).hdf5')
+!pip install -qq gradio
+import gradio as gr
+import numpy as np
 
+def recognize_digit(input):
+  input = input.reshape((1,28,28))
+  
+  prediction = np.squeeze(model.predict(input))
+  label = [0,1,2,3,4,5,6,7,8,9]
+  output = dict(zip(label, prediction.tolist()))
+  return output
+  
+gr.Interface(fn=recognize_digit, inputs="sketchpad", outputs="label").launch(debug = True)
